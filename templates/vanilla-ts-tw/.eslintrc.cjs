896e03d3f69e465064665ec8b85485f388daf22f
Javier Valero Cejudo <javalce29@gmail.com>
2024-06-28 13:16:40 +0200

    chore: Update dependencies and package versions

--- templates/vanilla-ts-tw/.eslintrc.cjs
+++ templates/vanilla-ts-tw/.eslintrc.cjs
@@ -4,10 +4,7 @@ const project = resolve(__dirname, 'tsconfig.eslint.json');
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  root: true,
   settings: {
     'import/resolver': {
       typescript: {
@@ -15,17 +12,10 @@ module.exports = {
       },
     },
   },
-  extends: [require.resolve('@vercel/style-guide/eslint/typescript')],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -35,17 +25,46 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
+    // General rules
     'padding-line-between-statements': [
       'warn',
-      { blankLine: 'always', prev: '*', next: ['return', 'export'] },
+      { blankLine: 'always', prev: '*', next: 'return' },
       { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
     ],
-    'no-console': 'warn',
+    // TypeScript rules
     '@typescript-eslint/no-shadow': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-confusing-void-expression': 'off',
+    // Import rules
+    'import/no-default-export': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: ['type', 'builtin', 'object', 'external', 'internal', 'parent', 'sibling', 'index'],
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    // Unicorn rules
+    'unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+        ignore: ['App.tsx'],
+      },
+    ],
   },
 };
